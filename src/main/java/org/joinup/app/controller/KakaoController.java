a8536d66bd18319637fe7ff0aee10a8e534cec8d
홍성채 <97865622+Hongsungchae@users.noreply.github.com>
2023-02-19 13:51:52 +0900

    kakao 로그인_카카오 이메일, 이름 받기 완

--- src/main/java/org/joinup/app/controller/KakaoController.java
+++ src/main/java/org/joinup/app/controller/KakaoController.java
@@ -1,6 +1,11 @@
 package org.joinup.app.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.joinup.app.domain.KakaoProfile;
+import org.joinup.app.domain.OAuthToken;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -11,6 +16,8 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,11 +40,52 @@ public class KakaoController {
         HttpEntity<MultiValueMap<String,String>> kakaoTokenRequest =
                 new HttpEntity<>(params,headers);
         //Http 요청하기 - POST방식으로 - 그리고 response 변수의 응답 받음.
-        ResponseEntity<String> reponse = rt.exchange("https://kauth.kakao.com/oauth/token",
+        ResponseEntity<String> response = rt.exchange("https://kauth.kakao.com/oauth/token",
                 HttpMethod.POST,
                 kakaoTokenRequest,
                 String.class);
 
-        return "카카오 인증 완료"+reponse;
+        //Gson, Json Simple, ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+        OAuthToken oauthToken = null;
+        try{
+         oauthToken = objectMapper.readValue(response.getBody(),OAuthToken.class);
+        }catch (JsonMappingException e){
+            e.printStackTrace();;
+        }catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+        System.out.println("oauthToken" + oauthToken);
+
+        RestTemplate rt2 = new RestTemplate();
+        //Httpheader 오브젝트 생성
+        HttpHeaders headers2 = new HttpHeaders();
+        headers2.add("Authorization","Bearer "+oauthToken.getAccess_token());
+        headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+        //HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+        HttpEntity<MultiValueMap<String,String>> kakaoProfileRequest =
+                new HttpEntity<>(headers2);
+        //Http 요청하기 - POST방식으로 - 그리고 response 변수의 응답 받음.
+        ResponseEntity<String> response2 = rt2.exchange(
+
+                "https://kapi.kakao.com/v2/user/me",
+                HttpMethod.POST,
+                kakaoProfileRequest,
+                String.class);
+
+        ObjectMapper objectMapper2 = new ObjectMapper();
+        KakaoProfile kakaoProfile = null;
+        try{
+            kakaoProfile = objectMapper2.readValue(response2.getBody(),KakaoProfile.class);
+        }catch (JsonMappingException e){
+            e.printStackTrace();;
+        }catch (JsonProcessingException e){
+            e.printStackTrace();
+        }
+        System.out.println("카카오 아이디" + kakaoProfile.kakao_account.getEmail());
+        System.out.println("카카오 이름" + kakaoProfile.getProperties());
+        UUID garbagePassword = UUID.randomUUID();
+        System.out.println("랜덤 비밀번호" + garbagePassword);
+        return "카카오 인증 완료"+response2.getBody();
     }
 }
