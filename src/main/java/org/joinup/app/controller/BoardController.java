0301566696b55c92e5b2455bdfc6660792c8c544
홍성채 <97865622+Hongsungchae@users.noreply.github.com>
2023-02-28 18:05:33 +0900

    게시판 셋팅(아직 X)

--- src/main/java/org/joinup/app/controller/BoardController.java
+++ src/main/java/org/joinup/app/controller/BoardController.java
@@ -4,9 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.joinup.app.domain.BoardDTO;
 import org.joinup.app.service.BoardService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,15 +19,22 @@ public class BoardController {
     private final BoardService boardService;
     @GetMapping("/boardWrite")
     public String saveBoard(){
-        return "boardWrite";
+        return "boardwrite";
     }
     @PostMapping("/boardWrite")
-    public String save(@ModelAttribute BoardDTO boardDTO){
+    public String save(@ModelAttribute BoardDTO boardDTO, HttpSession session){
+        boardDTO.setBoardWriter((String)(session.getAttribute("loginEmail")));
         int saveResult = boardService.save(boardDTO);
         if(saveResult > 0){
             return "index";
         }else{
-            return "index";
+            return "boardwrite";
         }
+    }
+    @GetMapping("/board")
+    public String findAll(Model model){
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList",boardDTOList);
+        return "board";
     }
 }
--- src/main/java/org/joinup/app/service/BoardService.java
+++ src/main/java/org/joinup/app/service/BoardService.java
@@ -5,11 +5,18 @@ import org.joinup.app.domain.BoardDTO;
 import org.joinup.app.repository.BoardRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
     public int save(BoardDTO boardDTO) {
         return boardRepository.save(boardDTO);
+    }
+
+    public List<BoardDTO> findAll() {
+        return boardRepository.findAll();
+
     }
 }
